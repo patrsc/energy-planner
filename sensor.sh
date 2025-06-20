a86ecd3ce21798fb80788825c43a005fe3011716
Patrick <patrsc@nubu.at>
2025-06-20 20:39:18 +0200

    Fix script

--- sensor.sh
+++ sensor.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 export STORAGE_DIR=/config/data/energy_planner
 export TZ=Europe/Vienna
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "$SCRIPT_DIR"
 python sensor.py "$@"
